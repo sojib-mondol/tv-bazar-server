b30de1788b7fd084b0ac46587bb02ebd146d4087
sojib-sudo <sojibmandol@gmail.com>
2022-11-25 21:49:08 +0600

    ledTV & Smart TV data collection created

--- index.js
+++ index.js
@@ -17,11 +17,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
+        // crt tv data
         const crtTVCollection = client.db('tv-bazarbd').collection('crtTvCollection');
+        // led tv data
+        const ledTVCollection = client.db('tv-bazarbd').collection('ledTvCollection');
+        // smart tv data
+        const smartTVCollection = client.db('tv-bazarbd').collection('smartTvCollection');
 
+        // gettung crt tv data
         app.get('/crtTvCollection', async(req, res) => {
             const query = {};
             const tv = await crtTVCollection.find(query).toArray();
+            res.send(tv);
+        })
+        // gettung led tv data
+        app.get('/ledTvCollection', async(req, res) => {
+            const query = {};
+            const tv = await ledTVCollection.find(query).toArray();
+            res.send(tv);
+        })
+        // gettung smart tv data
+        app.get('/smartTvCollection', async(req, res) => {
+            const query = {};
+            const tv = await smartTVCollection.find(query).toArray();
             res.send(tv);
         })
     }
